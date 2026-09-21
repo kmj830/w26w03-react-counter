a5c7269374aee701ace9438da08e6fe3139cc5ba
MinJung Kim <alswnd0830@naver.com>
2026-09-21 17:11:39 +0900

    Remove favicon.svg and update index.html to use favicon.png; add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/w26wo3-react-counter',
+  base: '/w26w03-react-counter/',
 })
